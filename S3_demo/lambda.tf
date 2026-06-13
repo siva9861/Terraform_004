52b17e933202a5364e8dd0b031ffd078d8e6a725
siva <siva@gmail.com>
2026-06-13 15:28:01 -0500

    fixed role in lambda

--- S3_demo/lambda.tf
+++ S3_demo/lambda.tf
@@ -2,7 +2,7 @@
 resource "aws_lambda_function" "example" {
   #filename         = data.archive_file.lambda_zip.output_path
   function_name    = "abc_publishGlueJobStatus"
-  role             = aws_iam_role.lambda_role.arn
+  role             = "arn:aws:iam::456568168979:role/service-role/abc_publishGlueJobStatus-role-oz2s81mp"
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.14"
   #source_code_hash = data.archive_file.lambda_zip.output_base64sha256
