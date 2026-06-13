f904d739e4517bfcbc465015c7c10ef7aca45922
siva9861 <siva498261@gmail.com>
2026-06-13 15:53:31 -0500

    Update lambda.tf

--- S3_demo/lambda.tf
+++ S3_demo/lambda.tf
@@ -1,9 +1,9 @@
 # Core Lambda Function Block
-resource "aws_lambda_function" "example" {
-  filename         = "glueJobStatus.py"
-  function_name    = "abc_publishGlueJobStatus"
-  role             = "arn:aws:iam::456568168979:role/service-role/abc_publishGlueJobStatus-role-oz2s81mp"
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.14"
+#resource "aws_lambda_function" "example" {
+ # filename         = "glueJobStatus.py"
+  #function_name    = "abc_publishGlueJobStatus"
+  #role             = "arn:aws:iam::456568168979:role/service-role/abc_publishGlueJobStatus-role-oz2s81mp"
+  #handler          = "lambda_function.lambda_handler"
+  #runtime          = "python3.14"
   #source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-}
+#}
